c32757de1f2679b5c1012ec51fe685eda5c30156
Janie Kim <janiekim@Janies-MBP.attlocal.net>
2020-11-10 11:33:08 -0600

    saving point

--- server/index.js
+++ server/index.js
@@ -1,14 +1,32 @@
+//A cookie is a small key and user data is stored in a database and not on the website anymore
+//There is one cookie per person per website
+//Sessions are kept on servers
+//Server receives a cookie from the browser
+//Sessions are empty objects, kept on servers and they correspond to a client's cookies
+//
+
 require('dotenv').config();
 const express = require('express');
 const massive = require('massive');
 // require session
+const session = require('express-session');
 const auth = require('./authController');
-const {SERVER_PORT, CONNECTION_STRING} = process.env;
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
 
 const app = express();
+
 // Top level middleware
 app.use(express.json());
+app.use(session({
+    resave: false, //should active sessions save if no changes made to them?
+    saveUninitialized: true, //should a new session save if no data was added?
+    secret: SESSION_SECRET, //random string to protect the cookie - this is stored in the .env file
+    cookies: { //max age of the cookie in milliseconds
+        maxAge: 1000 * 60 * 60 * 24 * 7 //milliseconds, seconds in a minute, seconds in an hour -- One Week Long
+    }
+}))
 
+//By invoking massive, we are connecting to the database
 massive({
     connectionString: CONNECTION_STRING,
     ssl: {
